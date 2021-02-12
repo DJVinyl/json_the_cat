84a199d2380bbc9f1644e8fb37fd74695400f6c2
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-02-12 00:15:37 +0000

    added mocha test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,22 @@
 const request = require("request");
-const args = process.argv.splice(2);
 
-const caturday = "https://api.thecatapi.com/v1/breeds/search?q=" + args[0];
+const fetchBreedDescription = (catBreed, callback) => {
+  const caturday = "https://api.thecatapi.com/v1/breeds/search?q=" + catBreed;
+  request(caturday, (error, response, body) => {
+    if (error) {
+      return callback(true, error);
+    }
+    if (response.statusCode !== 200) {
+      return callback(true,
+        Error(`Status Code ${response.statusCode} when fetching IP: ${body}`));
+    }
+    try {
+      const data = JSON.parse(body);
+      return callback(false, data[0].description);
+    } catch (err) {
+      return callback(true, `Description for ${catBreed} not found`);
+    }
+  });
+};
 
-request(caturday, (error, response, body) => {
-  console.log("error:", error); // Print the error if one occurred
-
-  if (error) {
-    return;
-  }
-  if (response.statusCode !== 200) {
-    Error(`Status Code ${response.statusCode} when fetching IP: ${body}`);
-    return;
-  }
-  try {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  } catch (err) {
-    throw `Description for ${args[0]} not found`;
-  }
-});
+module.exports = { fetchBreedDescription };
